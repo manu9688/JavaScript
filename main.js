1bbe9a2c60bb15d0cc9e999ebd150356bacc361d
Manuela Cardona <manuelita9688@gmail.com>
2025-09-26 10:41:50 +0200

    hola estoy haciendo ejercicios con javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Hola mundo desde JS");
+
+var nombre = 'Manuela';
+let apellido = 'Cardona Arias';
+const nie = '123456';
+console.log(nombre);
+console.log(typeof(nombre));
+const objeto = {name: 'Manuela', apellidos: 'Cardona Arias'};
+console.table(objeto);
+//usando alert
+alert(nombre);
+//usando prompt
+let saludo = prompt("Hola, escribe tu nombre");
+alert(saludo);
