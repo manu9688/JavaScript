c4153b541cce7d585fc4cda3ef5b377fbba50ded
Manuela Cardona <manuelita9688@gmail.com>
2025-09-28 22:37:39 +0200

    ejercicios classroom

--- main.js
+++ main.js
@@ -112,3 +112,80 @@ const pInfinitos = function (...datos){
 //llamada a la funcion para que se ejecute
 pInfinitos('1','2','3','4','5');
 
+// ?EJERCICIO
+/*
+1. A partir de la pizza sustituías todos los elemento del array por
+cervezas:🍔🌯🍣🍕🍜🍱🍙🍘🥩*/
+let emojis = ['🍔','🌯','🍣','🍕','🍜','🍱','🍙','🍘','🥩'];
+
+// Desde el for recorro el array de emojis desde la posición 4
+for (let i = 4; i < emojis.length; i++) {
+  emojis[i] ='🍺'; // sustituyo por cerveza
+}
+
+console.log(emojis); //muestro por consola el array
+
+/*
+2. Encontrar si existe un elemento en el array que sea una piña. 🍕🍕🍍🍕🍕*/
+//creo el array de pizza
+let pizza = ['🍕', '🍕', '🍍', '🍕', '🍕'];
+//creo una variable usando el indexOf que me indica si la piña exista
+//dando su posición en caso contrario mostrara un -1
+let pineapple = pizza.indexOf('🍍');
+
+console.log("La piña esta en la posición: "+pineapple); 
+
+/*3. Quita la piña del siguiente array. 🍕🍕🍍🍕🍕*/
+//creo el array
+let pizzaConP = ['🍕', '🍕', '🍍', '🍕', '🍕'];
+/*creo otra variable en donde guarde el array sin piña,
+el  metodo filter crea un nuevo array con los elementos que cumplan
+una condición en este caso que no tenga piña*/
+let sinP = pizzaConP.filter(fruta => fruta !== '🍍');
+
+console.log(sinP); //muestro el array sin piña
+/*
+4. A partir del siguiente array 🍓🍋🍓🍋🍓convierte todas las fresas en 🍄*/
+//array de frutas
+let fruits = ['🍓', '🍋', '🍓', '🍋', '🍓'];
+//uso un forEach para recorrer el array 
+fruits.forEach((fruit, index, arr) => { //recibo como parametros: el parametro donde guardo la fresa, el indice(posicion) y el array de frutas
+  if (fruit === '🍓') {//con el if comparo si hay una fresa
+    arr[index] = '🍄';// si la condicion se cumple lo reemplazo por hongo en la posicion que esta la fresa
+  }
+});
+
+console.log(fruits); //muestra el array con la fruta reemplazada
+
+/*
+5. Añade el siguiente icono 🥵 inmediatamente después del cada
+🌶️ en el siguiente array: 🌶️🥛🌶️🥛🌶️🥛*/
+//creación de array
+const ingredientes = ['🌶️', '🥛', '🌶️', '🥛', '🌶️', '🥛'];
+//array vacio donde se guardara el resultado
+const resultado = [];
+// bucle for para recprrer el array
+for (let i = 0; i < ingredientes.length; i++) {
+  resultado.push(ingredientes[i]);//uso del metodo push para añadir elementos al array
+  if (ingredientes[i] === '🌶️') { //condicional que verifica si esta el chile
+    resultado.push('🥵');//agrego carita si se cumple la condición
+  }
+}
+console.log(resultado);//muestro el resultado
+
+/*
+6. Añade una 🃏 cartas comodín entre medio de dos cartas . Por
+ejemplo:🎴🃏🎴 En el siguiente array: 🎴🎴🎴🃏🎴🎴🎴 */
+const cartas = ['🎴', '🎴', '🎴', '🃏', '🎴', '🎴', '🎴'];
+const result = [];
+
+for (let i = 0; i < cartas.length; i++) {
+  result.push(cartas[i]);
+
+  // Si hay dos 🎴 seguidas, añade 🃏 entre ellas
+  if (cartas[i] === '🎴' && cartas[i + 1] === '🎴') {
+    result.push('🃏');
+  }
+}
+
+console.log(result);
